ddb71a6090c0684fe696bbcaefd6cb9e6102c5cc
Klaudia Popow <401462@student.agh.edu.pl>
2023-01-01 23:45:38 +0100

    Add charts, and fix blocking messages in sprinkler script

--- sprinkler.py
+++ sprinkler.py
@@ -3,6 +3,7 @@ import time
 import paho.mqtt.client as mqtt
 import json
 import redis as redis
+from threading import Thread
 
 ''' JSON Message to sprinkler:
 {
@@ -11,42 +12,47 @@ import redis as redis
 }
 '''
 
-r = redis.Redis(host='localhost', port=6379, db=0)
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+def watering(redis):
+    while True:
+        time.sleep(0.2)
+        watering = str(redis.get("watering"))
+        if watering == "True":
+            humidity = int(r.get("humidity"))
+            if humidity == 100:
+                continue
+            humidity += 1
+            redis.set("humidity", humidity)
+            print("watering")
 
 
 def on_message(client, userdata, msg):
     payload = json.loads(msg.payload)
     print("Got msg: " + str(payload))
-
     if payload['turn']:
-        print("turning on watering for " + str(payload['time']) + " min")
-
-        end_time = datetime.datetime.now() + datetime.timedelta(minutes=payload['time'])
-        while True:
-            if datetime.datetime.now() >= end_time:
-                break
-            else:
-                humidity = int(r.get("humidity"))
-
-                if humidity == 100:
-                    continue
-
-                humidity += 1
-
-                r.set("humidity", humidity)
-
-                time.sleep(0.2)
-        print("watering finished")
-        return
-
-    if not payload['turn']:
+        if str(r.get("watering")) != "True":
+            print("turning on watering for " + str(payload['time']) + " min")
+            r.setex("watering", datetime.timedelta(minutes=payload['time']), value="True")
+        else:
+            print("Watering is turned on")
+    else:
         print("turning off watering")
+        r.set("watering", "False")
 
-        return
+# Sprinkler is separate thread that checks if "watering" key is set in redis
+# if yes it changes humidity value
 
+try:
+   t = Thread(target=watering, args=[r])
+   t.start()
+except:
+   print("Error: unable to start watering thread")
 
 client = mqtt.Client()
 client.on_message = on_message
 client.connect("localhost", 1883, 60)
 client.subscribe("sprinkler", 0)
 client.loop_forever()
+
+
